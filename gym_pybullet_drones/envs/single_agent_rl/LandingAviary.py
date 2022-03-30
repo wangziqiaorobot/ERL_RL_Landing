b51d4b2c846d74454f42cda1fb351d6602582e1c
ziqiao wang <wangziqiao30@gmail.com>
2022-03-30 23:13:40 +0200

    change the record flag

--- gym_pybullet_drones/envs/single_agent_rl/LandingAviary.py
+++ gym_pybullet_drones/envs/single_agent_rl/LandingAviary.py
@@ -17,7 +17,7 @@ class LandingAviary(BaseSingleAgentAviary):
                  freq: int=240,
                  aggregate_phy_steps: int=1,
                  gui=False,
-                 record=True, 
+                 record=False, 
                  obs: ObservationType=ObservationType.KIN,
                  act: ActionType=ActionType.LD
                  ):
