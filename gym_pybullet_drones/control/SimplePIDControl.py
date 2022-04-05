7c97af52032f4e21acd56b1857eedf8565da659d
wangziqiaorobot <wangziqiao6334@163.com>
2022-04-05 16:55:20 +0200

    change the PID

--- gym_pybullet_drones/control/SimplePIDControl.py
+++ gym_pybullet_drones/control/SimplePIDControl.py
@@ -41,7 +41,7 @@ class SimplePIDControl(BaseControl):
         #PD parameter of attitude controller
         self.P_COEFF_TOR =  np.array([0.15, 0.15, .08]) #np.array([0.3, 0.3, .08])#np.array([.9, .3, .05])
         #self.I_COEFF_TOR = np.array([.0001, .0001, .0001])
-        self.D_COEFF_TOR =  np.array([.05, .05, .3]) #np.array([.02, .02, .3])
+        self.D_COEFF_TOR =  np.array([.05, .05, .03]) #np.array([.02, .02, .3])
 
 
         self.MAX_ROLL_PITCH = np.pi/6
@@ -191,7 +191,7 @@ class SimplePIDControl(BaseControl):
         #### Target rotation #######################################
         target_rpy[0] = np.arcsin(-sign_z*target_force[1] / np.linalg.norm(target_force))
         target_rpy[1] = np.arctan2(sign_z*target_force[0], sign_z*target_force[2])
-        target_rpy[2] = math.pi/2
+        target_rpy[2] = math.pi/6
         target_rpy[0] = np.clip(target_rpy[0], -self.MAX_ROLL_PITCH, self.MAX_ROLL_PITCH)
         target_rpy[1] = np.clip(target_rpy[1], -self.MAX_ROLL_PITCH, self.MAX_ROLL_PITCH)
         cur_rotation = np.array(p.getMatrixFromQuaternion(cur_quat)).reshape(3, 3)
