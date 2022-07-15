1a7bd2077758b918d5685168eaade283346d0d69
wangziqiaorobot <wangziqiao6334@163.com>
2022-07-15 18:16:07 +0200

    changed the model.predect; changed some plots

--- experiments/learning/test_singleagent.py
+++ experiments/learning/test_singleagent.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     if algo == 'ppo':
         model = PPO.load(path)
     if algo == 'sac':
-        # model = SAC.load(path)
+        model = SAC.load(path)
 
         ###########
         weight = ARGS.exp+"/tb/SAC_1/Policy/iter_{0:05d}.pth".format(ARGS.iter)
@@ -78,9 +78,18 @@ if __name__ == "__main__":
         env_rms =ARGS.exp+"/RMS/iter_{0:05d}.npz".format(ARGS.iter)
         print("weight",weight)
         print("rms",env_rms)
-        device = get_device("cpu")
+
+
+        if torch.cuda.is_available():
+            device = "cuda:0"
+            print("GPU found")
+        else:
+            device = "cpu"
+        device="cpu"
+        # device = get_device("cpu")
+
         saved_variables = torch.load(weight, map_location=device)
-        
+        print("saved variables",saved_variables["data"])
         # Create policy object
         policy = RPGMlpPolicy(saved_variables["data"],device)
         
@@ -101,15 +110,20 @@ if __name__ == "__main__":
     test_env= LandingAviary(
                          gui=True,
                          record=True,
-                         aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS
+                         aggregate_phy_steps=shared_constants.AGGR_PHY_STEPS,
+                         filepath=ARGS.exp
                          )
     
     logger = Logger(logging_freq_hz=int(test_env.SIM_FREQ/test_env.AGGR_PHY_STEPS),
                      num_drones=1
                     )
-    # policy just in time compliation
-    dummy_inputs = torch.rand(1, test_env.observation_space, device=device)
-    policy = torch.jit.trace(policy, dummy_inputs)
+    # # policy just in time compliation
+    dummy_inputs = torch.rand(1, 25, device=device)
+    print("dummy input",dummy_inputs.size)
+
+    # policy = torch.jit.trace(policy, dummy_inputs)
+    # print("torch.jit policy",policy)
+
     test_env.load_rms(env_rms)
     #############
     obs = test_env.reset()
@@ -119,10 +133,12 @@ if __name__ == "__main__":
     
 
     # new log try ####
-    test_steps=800
+    test_steps=400
     actions = np.zeros(
         shape=(test_env.action_space.shape[0], test_steps), dtype=np.float32)
     observation = np.zeros(
+        shape=(test_env.observation_space.shape[0], test_steps), dtype=np.float32)
+    unnormalized_obs = np.zeros(
         shape=(test_env.observation_space.shape[0], test_steps), dtype=np.float32)
     rewards = np.zeros(
         shape=(1, test_steps), dtype=np.float32)
@@ -131,13 +147,21 @@ if __name__ == "__main__":
     infos = np.zeros(
         shape=(8, test_steps), dtype=np.float32)
     for i in range(test_steps):
+        # obs_numpy = np.array(obs) # construct your observation as a numpy array
+        # print("obs_numpy",obs_numpy)
+        # obs_tensor = torch.as_tensor(obs_numpy).to(device)
+        # print("obs_tensor",obs_tensor.size)
+        # action_numpy = policy.forward(obs_tensor).detach().cpu().numpy()
         
-        action, _states = policy.forward(obs
-                                        )
-        obs, reward, done, info = test_env.step(action)
+        action_numpy, _states = model.predict(obs)
+        obs, reward, done, info = test_env.step(action_numpy)
         test_env.render()
-        actions[:,i]=action
+        actions[:,i]=action_numpy
+        unnormalized_obs[:,i]=test_env._unnormalize_obs(obs,test_env.obs_rms)
+        print("unnormalized_obs",unnormalized_obs)
+        
         observation[:,i]=obs
+        print("observation",observation)
         rewards[:,i]=reward
         infos[:,i]=info
         time_plt[:,i]=i*test_env.AGGR_PHY_STEPS/240
@@ -152,119 +176,119 @@ if __name__ == "__main__":
     #     print(data['results'])
     #     print(data['ep_lengths'])
     
-    ############### Plot the states & actions
+#     ############### Plot the states & actions
     save_path = os.path.join(ARGS.exp)
-    plt.figure()
-    plt.plot(time_plt[0,:],test_env.GRAVITY*(0.1*actions[0,:]+1),label="thrust")
-    #plt.plot((actions[0,:]),label="b")
-    plt.grid()
-    plt.legend()
-    plt.title('action0_thrust')
-    plt.savefig(save_path + '/action0_thrust.jpg')
+#     plt.figure()
+#     plt.plot(time_plt[0,:],test_env.GRAVITY*(0.1*actions[0,:]+1),label="thrust")
+#     #plt.plot((actions[0,:]),label="b")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('action0_thrust')
+#     plt.savefig(save_path + '/action0_thrust.jpg')
     
-    plt.figure()
-    # plt.plot(test_env.MAX_THRUST/2*(actions[1,:]*0.05+1),label="roll")
-    plt.plot(time_plt[0,:],(actions[1,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="roll")
-    plt.grid()
-    plt.legend()
-    plt.title('action1_roll')
-    plt.savefig(save_path + '/action1_roll.jpg')
+#     plt.figure()
+#     # plt.plot(test_env.MAX_THRUST/2*(actions[1,:]*0.05+1),label="roll")
+#     plt.plot(time_plt[0,:],(actions[1,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="roll")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('action1_roll')
+#     plt.savefig(save_path + '/action1_roll.jpg')
 
-    plt.figure()
-    # plt.plot(test_env.MAX_THRUST/2*(actions[1,:]*0.05+1),label="roll")
-    plt.plot(time_plt[0,:],(actions[2,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="pitch")
-    plt.grid()
-    plt.legend()
-    plt.title('action2_pitch')
-    plt.savefig(save_path + '/action2_pitch.jpg')
+#     plt.figure()
+#     # plt.plot(test_env.MAX_THRUST/2*(actions[1,:]*0.05+1),label="roll")
+#     plt.plot(time_plt[0,:],(actions[2,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="pitch")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('action2_pitch')
+#     plt.savefig(save_path + '/action2_pitch.jpg')
 
-    plt.figure()
-    # plt.plot(test_env.MAX_THRUST/2*(actions[1,:]*0.05+1),label="roll")
-    plt.plot(time_plt[0,:],(actions[3,:]*test_env.MAX_ROLL_PITCH/math.pi/5*180),label="yaw")
-    plt.grid()
-    plt.legend()
-    plt.title('action3_yaw')
-    plt.savefig(save_path + '/action3_yaw.jpg')
+#     plt.figure()
+#     # plt.plot(test_env.MAX_THRUST/2*(actions[1,:]*0.05+1),label="roll")
+#     plt.plot(time_plt[0,:],(actions[3,:]*test_env.MAX_ROLL_PITCH/math.pi/5*180),label="yaw")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('action3_yaw')
+#     plt.savefig(save_path + '/action3_yaw.jpg')
 
-    ## observation
-    MAX_LIN_VEL_XY = 2 
-    MAX_LIN_VEL_Z = 1
+#     ## observation
+#     MAX_LIN_VEL_XY = 2 
+#     MAX_LIN_VEL_Z = 1
 
-    MAX_XY = 1
-    MAX_Z = 3
-    MAX_PITCH_ROLL = np.pi/2 # Full range
+#     MAX_XY = 1
+#     MAX_Z = 3
+#     MAX_PITCH_ROLL = np.pi/2 # Full range
     
 
-##### x\y\z
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[0,:]*MAX_XY,label="x")
-    plt.grid()
-    plt.legend()
-    plt.title('obs0_x')
-    plt.savefig(save_path + '/obs0_x.jpg')
+# ##### x\y\z
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[0,:]*MAX_XY,label="x")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('obs0_x')
+#     plt.savefig(save_path + '/obs0_x.jpg')
 
 
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[1,:]*MAX_XY,label="y")
-    plt.grid()
-    plt.legend()
-    plt.title('obs1_y')
-    plt.savefig(save_path + '/obs1_y.jpg')
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[1,:]*MAX_XY,label="y")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('obs1_y')
+#     plt.savefig(save_path + '/obs1_y.jpg')
 
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[2,:]*MAX_Z,label="z")
-    plt.grid()
-    plt.legend()
-    plt.title('obs2_z')
-    plt.savefig(save_path + '/obs2_z.jpg')
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[2,:]*MAX_Z,label="z")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('obs2_z')
+#     plt.savefig(save_path + '/obs2_z.jpg')
 
-##### r\p\y
+# ##### r\p\y
 
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[7,:]*MAX_PITCH_ROLL/math.pi*180,label="roll")
-    plt.plot(time_plt[0,:],(actions[1,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="command(action)_roll")
-    plt.grid()
-    plt.legend()
-    plt.title('obs3_roll')
-    plt.savefig(save_path + '/obs3_roll.jpg')
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[7,:]*MAX_PITCH_ROLL/math.pi*180,label="roll")
+#     plt.plot(time_plt[0,:],(actions[1,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="command(action)_roll")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('obs3_roll')
+#     plt.savefig(save_path + '/obs3_roll.jpg')
 
 
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[8,:]*MAX_PITCH_ROLL/math.pi*180,label="pitch")
-    plt.plot(time_plt[0,:],(actions[2,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="command(action)_pitch")
-    plt.grid()
-    plt.legend()
-    plt.title('obs4_pitch')
-    plt.savefig(save_path + '/obs4_pitch.jpg')
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[8,:]*MAX_PITCH_ROLL/math.pi*180,label="pitch")
+#     plt.plot(time_plt[0,:],(actions[2,:]*test_env.MAX_ROLL_PITCH/math.pi*180),label="command(action)_pitch")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('obs4_pitch')
+#     plt.savefig(save_path + '/obs4_pitch.jpg')
 
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[9,:]/math.pi*180,label="yaw")
-    plt.plot(time_plt[0,:],(actions[3,:]*test_env.MAX_ROLL_PITCH/math.pi/5*180),label="command(action)_yaw")
-    plt.grid()
-    plt.legend()
-    plt.title('obs5_yaw')
-    plt.savefig(save_path + '/obs5_yaw.jpg')
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[9,:]/math.pi*180,label="yaw")
+#     plt.plot(time_plt[0,:],(actions[3,:]*test_env.MAX_ROLL_PITCH/math.pi/5*180),label="command(action)_yaw")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('obs5_yaw')
+#     plt.savefig(save_path + '/obs5_yaw.jpg')
 
-### line_v
+# ### line_v
 
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[10,:]*MAX_LIN_VEL_XY,label="x_vel")
-    plt.plot(time_plt[0,:],observation[11,:]*MAX_LIN_VEL_XY,label="y_vel")
-    plt.plot(time_plt[0,:],observation[12,:]*MAX_LIN_VEL_Z,label="z_vel")
-    plt.grid()
-    plt.legend()
-    plt.title('lin_vel')
-    plt.savefig(save_path + '/lin_vel.jpg')
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[10,:]*MAX_LIN_VEL_XY,label="x_vel")
+#     plt.plot(time_plt[0,:],observation[11,:]*MAX_LIN_VEL_XY,label="y_vel")
+#     plt.plot(time_plt[0,:],observation[12,:]*MAX_LIN_VEL_Z,label="z_vel")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('lin_vel')
+#     plt.savefig(save_path + '/lin_vel.jpg')
 
-## ang_vel
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[13,:],label="x_ang_vel")
-    plt.plot(time_plt[0,:],observation[14,:],label="y_ang_vel")
-    plt.plot(time_plt[0,:],observation[15,:],label="z_ang_vel")
-    plt.grid()
-    plt.legend()
-    plt.title('ang_vel')
-    plt.savefig(save_path + '/ang_vel.jpg')
+# ## ang_vel
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[13,:],label="x_ang_vel")
+#     plt.plot(time_plt[0,:],observation[14,:],label="y_ang_vel")
+#     plt.plot(time_plt[0,:],observation[15,:],label="z_ang_vel")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('ang_vel')
+#     plt.savefig(save_path + '/ang_vel.jpg')
 
 ## rewards
 
@@ -283,19 +307,21 @@ if __name__ == "__main__":
     plt.savefig(save_path + '/reward.jpg')
     
     
-    MAX_F_XY=5  #max external froce in xy axis in robot frame
-    MAX_F_Z=11.76 ##max external froce in z axis in robot frame
-## force 
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[20,:]*MAX_F_XY,label="fx")
-    plt.plot(time_plt[0,:],observation[21,:]*MAX_F_XY,label="fy")
-    plt.plot(time_plt[0,:],observation[22,:]*MAX_F_Z,label="fz")
-    plt.grid()
-    plt.legend()
-    plt.title('Force')
-    plt.savefig(save_path + '/Force.jpg')
+#     MAX_F_XY=5  #max external froce in xy axis in robot frame
+#     MAX_F_Z=11.76 ##max external froce in z axis in robot frame
+# ## force 
+#     plt.figure()
+#     plt.plot(time_plt[0,:],observation[20,:]*MAX_F_XY,label="fx")
+#     plt.plot(time_plt[0,:],observation[21,:]*MAX_F_XY,label="fy")
+#     plt.plot(time_plt[0,:],observation[22,:]*MAX_F_Z,label="fz")
+#     plt.grid()
+#     plt.legend()
+#     plt.title('Force')
+#     plt.savefig(save_path + '/Force.jpg')
 
-## polt the observation
+
+
+####################### polt the observation ###############################
 ######## xyz
     plt.figure()
     plt.plot(time_plt[0,:],observation[0,:],label="obs0_x")
@@ -308,30 +334,25 @@ if __name__ == "__main__":
 
 ########   Q4
     plt.figure()
-    plt.plot(time_plt[0,:],observation[3,:],label="obs3_q1")
-    plt.plot(time_plt[0,:],observation[4,:],label="obs4_q2")
-    plt.plot(time_plt[0,:],observation[5,:],label="obs5_q3")
-    plt.plot(time_plt[0,:],observation[6,:],label="obs6_q4")
+    plt.plot(time_plt[0,:],observation[3,:],label="m1")
+    plt.plot(time_plt[0,:],observation[4,:],label="m2")
+    plt.plot(time_plt[0,:],observation[5,:],label="m3")
+    plt.plot(time_plt[0,:],observation[6,:],label="m4")
+    plt.plot(time_plt[0,:],observation[7,:],label="m5")
+    plt.plot(time_plt[0,:],observation[8,:],label="m6")
+    plt.plot(time_plt[0,:],observation[9,:],label="m7")
+    plt.plot(time_plt[0,:],observation[10,:],label="m8")
+    plt.plot(time_plt[0,:],observation[11,:],label="m9")
     plt.grid()
     plt.legend()
-    plt.title('obs_q4')
-    plt.savefig(save_path + '/obs_q4.jpg')
-
-######## RPY
-    plt.figure()
-    plt.plot(time_plt[0,:],observation[7,:],label="roll")
-    plt.plot(time_plt[0,:],observation[8,:],label="pitch")
-    plt.plot(time_plt[0,:],observation[9,:],label="yaw")
-    plt.grid()
-    plt.legend()
-    plt.title('obs_rpy')
-    plt.savefig(save_path + '/obs_rpy.jpg')
+    plt.title('obs_rotaion_matrix')
+    plt.savefig(save_path + '/obs_rotaion_matrix.jpg')
 
 ######## linear v
     plt.figure()
-    plt.plot(time_plt[0,:],observation[10,:],label="x")
-    plt.plot(time_plt[0,:],observation[11,:],label="y")
-    plt.plot(time_plt[0,:],observation[12,:],label="z")
+    plt.plot(time_plt[0,:],observation[12,:],label="V_x")
+    plt.plot(time_plt[0,:],observation[13,:],label="V_y")
+    plt.plot(time_plt[0,:],observation[14,:],label="V_z")
     plt.grid()
     plt.legend()
     plt.title('obs_lin_v')
@@ -339,9 +360,9 @@ if __name__ == "__main__":
 
 ######## anguler v
     plt.figure()
-    plt.plot(time_plt[0,:],observation[13,:],label="wx")
-    plt.plot(time_plt[0,:],observation[14,:],label="wy")
-    plt.plot(time_plt[0,:],observation[15,:],label="wz")
+    plt.plot(time_plt[0,:],observation[15,:],label="wx")
+    plt.plot(time_plt[0,:],observation[16,:],label="wy")
+    plt.plot(time_plt[0,:],observation[17,:],label="wz")
     plt.grid()
     plt.legend()
     plt.title('obs_ang_v')
@@ -349,10 +370,10 @@ if __name__ == "__main__":
 
 ########   action
     plt.figure()
-    plt.plot(time_plt[0,:],observation[16,:],label="a1")
-    plt.plot(time_plt[0,:],observation[17,:],label="a2")
-    plt.plot(time_plt[0,:],observation[18,:],label="a3")
-    plt.plot(time_plt[0,:],observation[19,:],label="a4")
+    plt.plot(time_plt[0,:],observation[18,:],label="a1")
+    plt.plot(time_plt[0,:],observation[19,:],label="a2")
+    plt.plot(time_plt[0,:],observation[20,:],label="a3")
+    plt.plot(time_plt[0,:],observation[21,:],label="a4")
     plt.grid()
     plt.legend()
     plt.title('obs_action')
@@ -360,14 +381,84 @@ if __name__ == "__main__":
 
 ######## force
     plt.figure()
-    plt.plot(time_plt[0,:],observation[20,:],label="fx")
-    plt.plot(time_plt[0,:],observation[21,:],label="fy")
-    plt.plot(time_plt[0,:],observation[22,:],label="fz")
+    plt.plot(time_plt[0,:],observation[22,:],label="fx")
+    plt.plot(time_plt[0,:],observation[23,:],label="fy")
+    plt.plot(time_plt[0,:],observation[24,:],label="fz")
     plt.grid()
     plt.legend()
     plt.title('obs_force')
     plt.savefig(save_path + '/obs_force.jpg')
 
+
+
+########## plot the unnormalized observations ###########
+
+
+    plt.figure()
+    plt.plot(time_plt[0,:],unnormalized_obs[0,:],label="obs0_x")
+    plt.plot(time_plt[0,:],unnormalized_obs[1,:],label="obs1_y")
+    plt.plot(time_plt[0,:],unnormalized_obs[2,:],label="obs2_z")
+    plt.grid()
+    plt.legend()
+    plt.title('unnormalized_obs_xyz')
+    plt.savefig(save_path + '/unnormalized_obs_xyz.jpg')
+
+########   Q4
+    plt.figure()
+    plt.plot(time_plt[0,:],unnormalized_obs[3,:],label="m1")
+    plt.plot(time_plt[0,:],unnormalized_obs[4,:],label="m2")
+    plt.plot(time_plt[0,:],unnormalized_obs[5,:],label="m3")
+    plt.plot(time_plt[0,:],unnormalized_obs[6,:],label="m4")
+    plt.plot(time_plt[0,:],unnormalized_obs[7,:],label="m5")
+    plt.plot(time_plt[0,:],unnormalized_obs[8,:],label="m6")
+    plt.plot(time_plt[0,:],unnormalized_obs[9,:],label="m7")
+    plt.plot(time_plt[0,:],unnormalized_obs[10,:],label="m8")
+    plt.plot(time_plt[0,:],unnormalized_obs[11,:],label="m9")
+    plt.grid()
+    plt.legend()
+    plt.title('unnormalized_obs_rotaion_matrix')
+    plt.savefig(save_path + '/unnormalized_obs_rotaion_matrix.jpg')
+
+######## linear v
+    plt.figure()
+    plt.plot(time_plt[0,:],unnormalized_obs[12,:],label="V_x")
+    plt.plot(time_plt[0,:],unnormalized_obs[13,:],label="V_y")
+    plt.plot(time_plt[0,:],unnormalized_obs[14,:],label="V_z")
+    plt.grid()
+    plt.legend()
+    plt.title('unnormalized_obs_lin_v')
+    plt.savefig(save_path + '/unnormalized_obs_lin_v.jpg')
+
+######## anguler v
+    plt.figure()
+    plt.plot(time_plt[0,:],unnormalized_obs[15,:],label="wx")
+    plt.plot(time_plt[0,:],unnormalized_obs[16,:],label="wy")
+    plt.plot(time_plt[0,:],unnormalized_obs[17,:],label="wz")
+    plt.grid()
+    plt.legend()
+    plt.title('unnormalized_obs_ang_v')
+    plt.savefig(save_path + '/unnormalized_obs_ang_v.jpg')
+
+########   action
+    plt.figure()
+    plt.plot(time_plt[0,:],unnormalized_obs[18,:],label="a1")
+    plt.plot(time_plt[0,:],unnormalized_obs[19,:],label="a2")
+    plt.plot(time_plt[0,:],unnormalized_obs[20,:],label="a3")
+    plt.plot(time_plt[0,:],unnormalized_obs[21,:],label="a4")
+    plt.grid()
+    plt.legend()
+    plt.title('unnormalized_obs_action')
+    plt.savefig(save_path + '/unnormalized_obs_action.jpg')
+
+######## force
+    plt.figure()
+    plt.plot(time_plt[0,:],unnormalized_obs[22,:],label="fx")
+    plt.plot(time_plt[0,:],unnormalized_obs[23,:],label="fy")
+    plt.plot(time_plt[0,:],unnormalized_obs[24,:],label="fz")
+    plt.grid()
+    plt.legend()
+    plt.title('unnormalized_obs_force')
+    plt.savefig(save_path + '/unnormalized_obs_force.jpg')
 
 
 
